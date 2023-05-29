640edc81f28f5c762c7d95e9242e1ef6c8b8775d
mael <mael.perrigaud@epitech.eu>
2023-05-29 20:11:08 +0200

    add honey.h for define the function that i used in other files

---
+++ includes/honey.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** honey
+** File description:
+** honey
+*/
+
+#pragma once
+
+int main(int ac, char** av);
+int print_help(char** av);
+int honey(char *file);
+char* verif_number_of_honey(char *nb);
+int manage_number_of_honey(char *nb);
+int transform_nb(char *nb);
