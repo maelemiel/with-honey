3f984bdd8b89bb121e5c3293f09657038e22ece5
mael <mael.perrigaud@epitech.eu>
2023-05-30 18:09:36 +0200

    remove function to other file

--- src/main.c
+++ src/main.c
@@ -11,19 +11,6 @@
 
 int main(int ac, char** av)
 {
-    if (ac != 2)
-        return 0;
-    print_help(av);
-    printf("nb : %d\n", manage_number_of_honey(av[1]));
-    return 0;
-}
-
-int print_help(char** av)
-{
-    if (av[1][0] == '-' && av[1][1] == 'h') {
-        printf("USAGE\n\t./honey number of grams of honey \n\nDESCRIPTION\n\t"
-        "once done, recipes will be proposed to you\n\n");
-        exit (0);
-    }
+    manage_args(ac, av);
     return 0;
 }
