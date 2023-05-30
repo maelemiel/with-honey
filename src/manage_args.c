6940200c4163556c15a150d7d260cee073031027
mael <mael.perrigaud@epitech.eu>
2023-05-30 18:08:36 +0200

    add function for do all

---
+++ src/manage_args.c
@@ -0,0 +1,18 @@
+/*
+** PROJECT, 2023
+** honey
+** File description:
+** manage_args
+*/
+
+#include <stdio.h>
+#include "honey.h"
+
+int manage_args(int ac, char** av)
+{
+    verif_args(ac);
+    print_help(av);
+    printf("nb : %d %s\n", manage_number_of_honey(av[1]), verif_units(av[2]));
+    fflush(stdout);
+    return 0;
+}
