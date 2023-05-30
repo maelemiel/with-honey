9eab3e3a2d9c10839dc7bf485a509e7572c6e6f2
mael <mael.perrigaud@epitech.eu>
2023-05-30 12:04:09 +0200

    edit the main to be better file

--- src/main.c
+++ src/main.c
@@ -1,19 +1,19 @@
 /*
-** EPITECH PROJECT, 2023
+** PROJECT, 2023
 ** honey
 ** File description:
 ** main
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "honey.h"
 
 int main(int ac, char** av)
 {
     if (ac != 2)
-        return 84;
-    if (print_help(av) == 0)
         return 0;
+    print_help(av);
     printf("nb : %d\n", manage_number_of_honey(av[1]));
     return 0;
 }
@@ -22,8 +22,8 @@ int print_help(char** av)
 {
     if (av[1][0] == '-' && av[1][1] == 'h') {
         printf("USAGE\n\t./honey number of grams of honey \n\nDESCRIPTION\n\t"
-        "once done, recipes will be proposed to you");
-        return 0;
+        "once done, recipes will be proposed to you\n\n");
+        exit (0);
     }
-    return 84;
+    return 0;
 }
