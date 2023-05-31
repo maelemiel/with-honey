07edddc93cb83ca94830abb6188d28ba4e44b891
mael <mael.perrigaud@epitech.eu>
2023-05-31 19:17:20 +0200

    add function to init the struct

--- src/main.c
+++ src/main.c
@@ -8,9 +8,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "honey.h"
+#include "struct.h"
+
+static void inti_struct(parser_t *parser)
+{
+    parser->recipe = malloc(sizeof(struct Recipe));
+    parser->recipe->name = NULL;
+    parser->recipe->ingredients = NULL;
+    parser->recipe->preparation = NULL;
+    parser->recipe->cooking = NULL;
+    parser->recipe->rest = NULL;
+    parser->recipe->nb = NULL;
+    parser->recipe->unit = NULL;
+    parser->recipe->time = NULL;
+    parser->recipe->difficulty = NULL;
+    parser->recipe->price = NULL;
+    parser->recipe->type = NULL;
+}
 
 int main(int ac, char** av)
 {
+    parser_t *parser = malloc(sizeof(parser_t));
+    inti_struct(parser);
     manage_args(ac, av);
     return 0;
 }
