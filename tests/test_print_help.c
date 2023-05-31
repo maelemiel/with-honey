90c839453f11c04ac80ce43db7997e3b670a45d5
mael <mael.perrigaud@epitech.eu>
2023-05-31 16:33:56 +0200

    edit the test for the new -h

--- tests/test_print_help.c
+++ tests/test_print_help.c
@@ -15,7 +15,8 @@ Test(honey_help, test_honey_print)
 
     cr_redirect_stdout();
     cr_assert_eq(print_help(av), 0);
-    cr_assert_stdout_eq_str("USAGE\n\t./honey number of grams of honey \n\n"
-    "DESCRIPTION\n\tonce done, recipes will be proposed to you\n\n");
+    cr_assert_stdout_eq_str("USAGE\n\t./honey [number] [unit]\n\nDESCRIPTION\n\t"
+    "number\t\ta number of units\n\tunit\t\ta unit among g, kg, l, cl, ml\n\n"
+    "EXAMPLE\n\t./honey 42 kg\n");
 }
 
