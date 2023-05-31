6fd7d92ed0edd2d722e691008142f54fb474cab2
mael <mael.perrigaud@epitech.eu>
2023-05-31 19:15:49 +0200

    edit name of the function and parameters

--- includes/honey.h
+++ includes/honey.h
@@ -9,7 +9,7 @@
 
 int main(int ac, char** av);
 int print_help(char** av);
-int honey(char *file);
+int check_file_in_the_folder(char *path_folder);
 char* verif_number_of_honey(char *nb);
 int manage_number_of_honey(char *nb);
 int transform_nb(char *nb);
