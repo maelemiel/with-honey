026671dd116895bd878aa499c7f19269aa1b35d6
mael <mael.perrigaud@epitech.eu>
2023-05-31 19:43:44 +0200

    add new function to print the args honey

--- src/manage_args.c
+++ src/manage_args.c
@@ -12,8 +12,14 @@ int manage_args(int ac, char** av)
 {
     verif_args(ac);
     print_help(av);
+    print_honey(av);
+    check_file_in_the_folder("./recipes");
+    return 0;
+}
+
+int print_honey(char **av)
+{
     printf("nb : %d %s\n", manage_number_of_honey(av[1]), verif_units(av[2]));
     fflush(stdout);
-    check_file_in_the_folder("./recipes");
     return 0;
 }
