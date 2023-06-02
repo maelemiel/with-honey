dbe1cbf8ae0dd734b44c9a43886dda65af6144fb
mael <mael.perrigaud@epitech.eu>
2023-06-02 19:26:44 +0200

    start the parser by open all the files in the folder parser

--- src/parser/take_file.c
+++ src/parser/take_file.c
@@ -6,6 +6,7 @@
 */
 
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -21,10 +22,32 @@ int check_file_in_the_folder(char *path_folder)
         while ((dir = readdir(d)) != NULL) {
             if (dir->d_name[0] != '.') {
                 printf("%s\n", dir->d_name);
+                parse_file(dir->d_name, path_folder);
                 nb++;
             }
         }
         closedir(d);
     printf("nb : %d\n", nb);
+    return 0;
+}
+
+int parse_file(char *file, char *path_folder)
+{
+    struct stat s;
+    int fd = 0;
+    char *buffer = NULL;
+    char *fwd = malloc(sizeof(char) * 10000);
+    strcat(fwd, path_folder);
+    strcat(fwd, "/");
+    strcat(fwd, file);
+
+    printf("%s\n", fwd);
+    stat(fwd, &s);
+    buffer = malloc(sizeof(char) * (s.st_size + 1));
+    fd = open(fwd, O_RDONLY);
+    read(fd, buffer, s.st_size);
+    buffer[s.st_size] = '\0';
+    printf("%s\n", "jesuisla");
+    printf("%s\n", buffer);
     return 0;
 }
